26a522624db95d113c173fd368c11d5f3d57ea98
Pedro Castro <pedro.castro@koerber.com>
2023-01-02 15:12:57 +0000

    Debug

--- ci_cd/docker_build/script.py
+++ ci_cd/docker_build/script.py
@@ -20,11 +20,11 @@ clientDocker.images.build(
         buildargs=json.loads(build_args)
 )
 
-clientDocker.login(
-        username=credentials["clientId"],
-        password=credentials["clientSecret"],
-        registry=acr_endpoint
-    )
+# clientDocker.login(
+#         username=credentials["clientId"],
+#         password=credentials["clientSecret"],
+#         registry=acr_endpoint
+#     )
 # try:
 #     pushResponse = clientDocker.api.push(
 #         repository=build_repo_path,
