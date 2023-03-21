b446fec9294de2bbcaea8a4371e46fa0236e399e
Pedro Daniel Almeida Castro <pedro.castro@koerber.com>
2023-03-21 16:08:59 +0000

    Add Trivt

--- ci_cd/docker_build/script.py
+++ ci_cd/docker_build/script.py
@@ -1,9 +1,21 @@
-import os, docker, json
+import os, docker, json, sys
 acr_endpoint = os.environ['INPUT_ACR_ENDPOINT']
 build_path = os.environ['INPUT_DOCKERBUILD_PATH']
 build_repo = os.environ['INPUT_DOCKERBUILD_REPO']
 build_tag = os.environ['INPUT_DOCKERBUILD_TAG']
 build_args = os.environ['INPUT_DOCKERBUILD_ARGS']
+
+try:
+    docker_scan = os.environ['INPUT_DOCKER_SCAN']
+except:
+    docker_scan = "false"
+
+try:
+    docker_trivy_image_flags = os.environ['INPUT_DOCKER_TRIVY_IMAGE_FLAGS']
+    docker_trivy_vulnerability_ignore = os.environ['INPUT_DOCKER_TRIVY_VULNERABILITY_IGNORE']
+except:
+    docker_trivy_image_flags = "--exit-code 1 --severity HIGH,CRITICAL --ignore-unfixed"
+    docker_trivy_vulnerability_ignore = ""
 
 try:
     azure_credentials = os.environ['INPUT_AZURE_CREDENTIALS']
@@ -29,6 +41,17 @@ clientDocker.images.build(
         tag=build_complete_path,
         buildargs=json.loads(build_args)
 )
+
+if docker_scan == "true":
+    vulnerability_ignored = docker_trivy_vulnerability_ignore.split(",")
+    ignore_vun = open(".trivyignore","w")
+    for vulnerability in vulnerability_ignored:
+        ignore_vun.write(vulnerability + "\n")
+    ignore_vun.close()
+    exit_code_os = os.system('trivy image' + ' ' + docker_trivy_image_flags + ' ' + build_complete_path)
+    exit_code = exit_code_os >> 8
+    if exit_code != 00000000: 
+        sys.exit('Something bad happened')
 
 if azure_credentials != "NULL" :
     credentials = json.loads(azure_credentials)
