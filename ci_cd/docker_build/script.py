c3eea1d19b10ea11a3163b4f5f5d35a18a9ad811
Herculano Oliveira <herculano.oliveira@vaibe.com>
2023-03-22 11:23:06 +0000

    Test

--- ci_cd/docker_build/script.py
+++ ci_cd/docker_build/script.py
@@ -1,4 +1,6 @@
 import os, docker, json, sys
+from subprocess import run
+
 acr_endpoint = os.environ['INPUT_ACR_ENDPOINT']
 build_path = os.environ['INPUT_DOCKERBUILD_PATH']
 build_repo = os.environ['INPUT_DOCKERBUILD_REPO']
@@ -34,45 +36,50 @@ try:
 except:
     print("There was an error using servers docker. Please check if docker is running.")
 
-os.environ['GITHUB_OUTPUT'] = "It is work"
-print("It is work")
+build_repo_path = acr_endpoint + '/' + build_repo
+build_complete_path = build_repo_path + ':' + build_tag
+build_complete_path = build_complete_path.lower()
+clientDocker.images.build(
+        path=build_path,
+        tag=build_complete_path,
+        buildargs=json.loads(build_args)
+)
 
-# build_repo_path = acr_endpoint + '/' + build_repo
-# build_complete_path = build_repo_path + ':' + build_tag
-# build_complete_path = build_complete_path.lower()
-# clientDocker.images.build(
-#         path=build_path,
-#         tag=build_complete_path,
-#         buildargs=json.loads(build_args)
-# )
+if docker_scan == "true":
+    vulnerability_ignored = docker_trivy_vulnerability_ignore.split(",")
+    ignore_vun = open(".trivyignore","w")
+    for vulnerability in vulnerability_ignored:
+        ignore_vun.write(vulnerability + "\n")
+    ignore_vun.close()
 
-# if docker_scan == "true":
-#     vulnerability_ignored = docker_trivy_vulnerability_ignore.split(",")
-#     ignore_vun = open(".trivyignore","w")
-#     for vulnerability in vulnerability_ignored:
-#         ignore_vun.write(vulnerability + "\n")
-#     ignore_vun.close()
-#     exit_code_os = os.system('trivy image' + ' ' + docker_trivy_image_flags + ' ' + build_complete_path)
-#     exit_code = exit_code_os >> 8
-#     if exit_code != 00000000: 
-#         sys.exit('Something bad happened')
-#     os.environ['GITHUB_OUTPUT'] = "It is work"
+    # exit_code_os = os.system('trivy image' + ' ' + docker_trivy_image_flags + ' ' + build_complete_path)
+    # exit_code = exit_code_os >> 8
+    # if exit_code != 00000000: 
+    #     sys.exit('Something bad happened')
 
-# if azure_credentials != "NULL" :
-#     credentials = json.loads(azure_credentials)
-#     clientDocker.login(
-#         username=credentials["clientId"],
-#         password=credentials["clientSecret"],
-#         registry=acr_endpoint
-#     )
-# else:
-#     clientDocker.login(
-#             username=azure_username,
-#             password=azure_password,
-#             registry=acr_endpoint
-#     )
+    p = run( 
+            'trivy', \
+            'image', \
+            docker_trivy_image_flags, \
+            build_complete_path )
+    if p.returncode != 0:
+        print('Something bad happened')
 
-# pushResponse = clientDocker.api.push(
-#     repository=build_repo_path,
-#     tag=build_tag,
-# )
+if azure_credentials != "NULL" :
+    credentials = json.loads(azure_credentials)
+    clientDocker.login(
+        username=credentials["clientId"],
+        password=credentials["clientSecret"],
+        registry=acr_endpoint
+    )
+else:
+    clientDocker.login(
+            username=azure_username,
+            password=azure_password,
+            registry=acr_endpoint
+    )
+
+pushResponse = clientDocker.api.push(
+    repository=build_repo_path,
+    tag=build_tag,
+)
