d22a1d07090b679775b9b3be6706ecd922bf3f67
Pedro Daniel Almeida Castro <pedro.castro@koerber.com>
2023-05-12 11:36:37 +0100

    Test new docke approach

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-ROOT_DIR=/etc/nginx/conf.d/
-
-echo "Replacing env constants in JS"
-for file in $(find . -name '*.conf');
+echo "Replacing env constants in conf file"
+for file in $(find /etc/nginx/conf.d -name '*.conf');
 do
   echo "Processing $file ...";
 
   sed -i 's|SYSTEM_ENV_BASE_ZONE|'${SYSTEM_ENV_BASE_ZONE}'|g' $file
 
 done
-
-# Starting NGINX
-nginx -g 'daemon off;'
